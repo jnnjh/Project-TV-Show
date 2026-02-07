211806069fd683373defbbeace66ee53c9c8ce83
jnnjh <joannejhoieacar@gmail.com>
2026-02-07 14:10:29 +0100

    rendered all episodes and edited the style.

--- script.js
+++ script.js
@@ -4,9 +4,36 @@ function setup() {
   makePageForEpisodes(allEpisodes);
 }
 
-function makePageForEpisodes(episodeList) {
+/*function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
   rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+
+
+}*/
+
+function makePageForEpisodes(episodeList) {
+  const rootElem = document.getElementById("root");
+  rootElem.innerHTML = "";
+
+  episodeList.forEach(episode => {
+    // to create zero-padded episode code
+    const season = String(episode.season).padStart(2, "0");
+    const number = String(episode.number).padStart(2, "0");
+    const episodeCode = `S${season}E${number}`;
+
+    // to create episode container
+    const episodeDiv = document.createElement("div");
+
+    episodeDiv.innerHTML = `
+      <h2>${episode.name} - ${episodeCode}</h2>
+      <img src="${episode.image.medium}" alt="${episode.name}">
+      <p>${episode.summary}</p>
+    `;
+
+    rootElem.appendChild(episodeDiv);
+  });
 }
 
+
 window.onload = setup;
+
