82beb6dd69ce94de958a6f170b6ebb498a7eb7a9
Fanglai <laura@FLMacBook-Air.local>
2026-02-08 17:51:23 +0100

    finished level200

--- script.js
+++ script.js
@@ -10,11 +10,18 @@ function setup() {
 
 
 }*/
+const state={
+  allEpisodes:getAllEpisodes(),
+  searchTerm:""
+}
+const rootElem = document.getElementById("root");
+const episodeAmount=document.querySelector("#episodeAmount")
+const episodeSearch=document.querySelector(".episode-search")
+const selectEpisode=document.querySelector("#select")
 
 function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  rootElem.innerHTML = "";
-
+  episodeAmount.textContent=`Displaying ${episodeList.length}/${state.allEpisodes.length}`
+  episodeSearch.append(episodeAmount)
   episodeList.forEach(episode => {
     // to create zero-padded episode code
     const season = String(episode.season).padStart(2, "0");
@@ -32,8 +39,42 @@ function makePageForEpisodes(episodeList) {
 
     rootElem.appendChild(episodeDiv);
   });
+ 
+}
+function render() {
+  const filteredEpisodes = document.querySelector("#root");
+  filteredEpisodes.innerHTML = "";
+  const filtered = state.allEpisodes.filter((episode) =>
+    episode.name.toLowerCase().includes(state.searchTerm.toLowerCase())||
+    episode.summary.toLowerCase().includes(state.searchTerm.toLowerCase())
+  );
+
+  makePageForEpisodes(filtered);
 }
 
+const handleInput = (event) => {
+  state.searchTerm = event.target.value;
+  selectEpisode.value = "Select an episode"
+  render();
+};
+
+const searchBox = document.getElementById("search");
+searchBox.addEventListener("input", handleInput);
+
+state.allEpisodes.forEach((episode,index) => {
+    const season = String(episode.season).padStart(2, "0");
+    const number = String(episode.number).padStart(2, "0");
+    const episodeCode = `S${season}E${number}`;
+    const option=document.createElement('option')
+    option.value=index
+    option.textContent=`${episodeCode} - ${episode.name}`
+    selectEpisode.appendChild(option)
+})
+selectEpisode.addEventListener("change",(event)=>{
+  const index = event.target.value;
+  rootElem.innerHTML = "";
+  makePageForEpisodes([state.allEpisodes[index]]);
+});
 
 window.onload = setup;
 
